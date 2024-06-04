31cd6d0e4c89c255e51177d4e456e59ed506b71a
Mohammad Ricko Aprilianto <ricko.aprilianto00@gmail.com>
2024-06-04 20:38:29 +0800

    [ADD] PelaporController and make method dashboardLapor, laporanAnda

--- app/Http/Controllers/PelaporController.php
+++ app/Http/Controllers/PelaporController.php
@@ -4,23 +4,79 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Laporan;
+use App\Models\User_Snapshot;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\URL;
 use App\Helper\ApiFormatter;
 use Exception;
+use Carbon\Carbon;
 
 class PelaporController extends Controller
 {
     public function dashboardPelapor(){
 
         try {
+            Carbon::setLocale('id');
+
             $namaUser = auth()->user()->name;
 
-            $laporanTerakhir = Laporan::orderBy('tgl_lapor', 'desc')->get();
+            $laporanTerakhirDashboard = Laporan::with('galleries')
+                ->orderBy('tgl_lapor', 'desc')
+                ->take(10)->get();
 
             $data = [
                 'namaUser' => $namaUser,
-                'laporanTerakhir' => $laporanTerakhir
+                'laporanTerakhirDashboard' => $laporanTerakhirDashboard->map(function ($laporan){
+                    $firstGallery = $laporan->galleries->first();
+
+                    return [
+                        'id' => $laporan->id,
+                        'deskripsi_lapor' => $laporan->deskripsi,
+                        'lokasi_kejadian' => $laporan->lokasi_kejadian,
+                        'waktu_lalu' => Carbon::parse($laporan->tgl_lapor)->diffForHumans(),
+                        'gambar_laporan' => $firstGallery->file_name,
+                        'maps' => $laporan->maps
+                    ];
+
+                }),
+            ];
+
+            return ApiFormatter::createApi(200, 'success', $data);
+
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(401, 'failed', $error);
+        }
+
+    }
+
+
+    public function laporanAnda(){
+        
+        try {
+            Carbon::setLocale('id');
+
+            $laporanAnda = Laporan::with(['userSnapshot','galleries'])
+                ->where('user__snapshot_id', auth()->user()->id)
+                ->orderBy('tgl_lapor', 'desc')
+                ->get();
+
+            $data = [
+                'laporanAnda' => $laporanAnda->map(function ($laporan){
+                    $firstGallery = $laporan->galleries->first();
+
+                    return [
+                        'id' => $laporan->id,
+                        'deskripsi_lapor' => $laporan->deskripsi,
+                        'status_laporan' => $laporan->status_lapor,
+                        'role_pelapor' => $laporan->userSnapshot->role,
+                        'nama_pelapor' => $laporan->userSnapshot->name,
+                        'lokasi_kejadian' => $laporan->lokasi_kejadian,
+                        'waktu_lalu' => Carbon::parse($laporan->tgl_lapor)->diffForHumans(),
+                        'maps' => $laporan->maps,
+                        'gambar_laporan' => $firstGallery->file_name
+                    ];
+
+                }),
             ];
 
             return ApiFormatter::createApi(200, 'success', $data);
