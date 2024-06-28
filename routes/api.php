2de89943565546d4bc48f9fb4568bc24f30ff7c9
Mohammad Ricko Aprilianto <ricko.aprilianto00@gmail.com>
2024-06-28 20:11:35 +0800

    login api

--- routes/api.php
+++ routes/api.php
@@ -22,22 +22,7 @@ use App\Http\Controllers\AdminController;
 */
 
 Route::middleware(['auth:sanctum', 'pelapor'])->group(function () {
-    
-
-});
-Route::middleware(['auth:sanctum', 'keamanan'])->group(function () {
-});
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    
-
-});
-
-Route::post('login', [AuthController::class, 'login']);
-Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::post('register', [AuthController::class, 'registerUser']);
-
-
-Route::get('profile/pelapor', [ProfileController::class, 'profile']);
+    Route::get('profile/pelapor', [ProfileController::class, 'profile']);
     Route::patch('profile/pelapor/update', [ProfileController::class, 'updateProfile']);
     Route::get('laporan/pelapor', [UserController::class, 'laporanAnda']);
     Route::get('laporan/all/pelapor', [LaporanController::class, 'index']);
@@ -47,16 +32,16 @@ Route::get('profile/pelapor', [ProfileController::class, 'profile']);
     // Route::post('post-lapor', [LaporanController::class, 'store']);
     Route::get('dashboard/pelapor', [UserController::class, 'dashboardPelapor']);
 
-
-    
+});
+Route::middleware(['auth:sanctum', 'keamanan'])->group(function () {
     Route::get('profile/petugas', [ProfileController::class, 'profile']);
     Route::post('updateprofil-pelapor', [ProfileController::class, 'updateProfile']);
     Route::get('laporan/petugas', [UserController::class, 'laporanAnda']);
     Route::get('laporan/all/petugas', [LaporanController::class, 'index']);
     Route::get('laporan/{id}/petugas', [LaporanController::class, 'show']);
     Route::get('dashboard/petugas', [UserController::class, 'dashboardPelapor']);
-
-
+});
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('profile/admin', [ProfileController::class, 'profile']);
     Route::post('updateprofil-pelapor', [ProfileController::class, 'updateProfile']);
     Route::get('laporan/all/admin', [LaporanController::class, 'index']);
@@ -64,3 +49,9 @@ Route::get('profile/pelapor', [ProfileController::class, 'profile']);
     Route::get('dashboard/admin', [AdminController::class, 'dashboardAdmin']);
     Route::get('akun/all', [AdminController::class, 'getSemuaAkun']);
     Route::get('akun/{id}', [AdminController::class, 'detailAkun']);
+
+});
+
+Route::post('login', [AuthController::class, 'login']);
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('register', [AuthController::class, 'registerUser']);
